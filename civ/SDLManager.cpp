4af0c4a82cb8c6dd8fc27e77b2286614c171c2af
jakob <jakobfischer93@gmail.com>
2016-06-26 15:38:19 +0200

    Small changes

--- civ/SDLManager.cpp
+++ civ/SDLManager.cpp
@@ -26,7 +26,7 @@ SDL::SDL(const string& windowName, const Rect& screen):
 	if (SDL_SetRenderDrawBlendMode(renderer, SDL_BLENDMODE_BLEND) == -1)
 		throw SDLException("SDL_SetRenderDrawBlendMode");
 
-		common::Log("Renderer created!");
+	common::Log("Renderer created!");
 }
 
 SDL::~SDL(void){
